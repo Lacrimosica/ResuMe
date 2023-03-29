7d305cef1e39eee1c34ebdf939f7cc3763f1dc65
ORBYTA\mohammad.goudarzi <mohammad.goudarzi@orbyta.it>
2023-03-29 13:29:56 +0200

    Spring init

---
+++ src/main/java/com/resume/resumetool/ResumetoolApplication.java
@@ -0,0 +1,13 @@
+package com.resume.resumetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumetoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resume/resumetool/ResumetoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.resumetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumetoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
